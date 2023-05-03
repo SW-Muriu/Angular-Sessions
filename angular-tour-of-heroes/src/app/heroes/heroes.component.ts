cf18e3f30d558bdf41865cc392e3d26be39febbb
SW-Muriu <sw.muriu@gmail.com>
2023-05-03 16:53:13 +0300

    Styled the name of the heroes, one might say gave em a suit

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -8,6 +8,13 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
+
 heroes = HEROES;
+selectedHero?: Hero;
+
+onSelect(hero:Hero): void
+{
+  this.selectedHero = hero;
+}
 }
 
