1fa63708eef28244a59e79b8a94fdc1007ef301c
SW-Muriu <sw.muriu@gmail.com>
2023-05-04 11:29:59 +0300

    Made changes that work, I dont know why they do yet

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,14 +1,11 @@
 import { Component, Input } from '@angular/core';
-import { Hero } from '../hero'; 
+import { Hero } from '../hero';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
   styleUrls: ['./hero-detail.component.css']
 })
-
-
-
-export class HeroDetailComponent 
-{
-  @Input () hero?: Hero;
+export class HeroDetailComponent {
+  @Input() hero?: Hero;
 }
