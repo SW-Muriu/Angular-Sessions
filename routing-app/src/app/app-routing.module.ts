b98efefcf9bab52c71c9bc287a3066a5844177fe
SW-Muriu <sw.muriu@gmail.com>
2023-05-08 08:53:36 +0300

    Link Activation through router-outlets

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = // sets up routes constant where you define your routes
+[
+  {
+    path: 'first-component', component: FirstComponent
+  },
+  {
+    path: 'second-component', component: SecondComponent
+  },
+]; 
 
+//Configures NGModules and imports
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
