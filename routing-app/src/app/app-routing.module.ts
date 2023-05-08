e767e297c984b8e73d20d6909e6d40621b1f67f4
SW-Muriu <sw.muriu@gmail.com>
2023-05-08 11:16:17 +0300

    Messed up my routinggit add .Gotta take a break

--- routing-app/src/app/app-routing.module.ts
+++ routing-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = // sets up routes constant where you define your routes
 [
@@ -11,7 +14,19 @@ const routes: Routes = // sets up routes constant where you define your routes
   {
     path: 'second-component', component: SecondComponent
   },
-]; 
+  {
+    path: '**', component: PageNotFoundComponent //Wildcard route for a 404 page
+  }
+  
+]; bootstrapApplication(AppComponent),
+{
+  providers:
+[
+  provideRouter(routes, WithComponentInputBinding())
+]
+}
+
+
 
 //Configures NGModules and imports
 @NgModule({
@@ -19,3 +34,7 @@ const routes: Routes = // sets up routes constant where you define your routes
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+function WithComponentInputBinding(): import("@angular/router").RouterFeatures {
+  throw new Error('Function not implemented.');
+}
+
