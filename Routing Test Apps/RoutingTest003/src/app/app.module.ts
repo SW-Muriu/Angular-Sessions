902d6a4891ffe86046f54c3f0a802b5b76b9e430
SW-Muriu <sw.muriu@gmail.com>
2023-05-08 14:40:00 +0300

    Initial Commit

--- Routing Test Apps/RoutingTest003/src/app/app.module.ts
+++ Routing Test Apps/RoutingTest003/src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { Routes } from '@angular/router';
+
+const routes: Routes = 
+[
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: ForgotPasswordComponent }, 
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ Routing Test Apps/RoutingTest003/src/app/components/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+
+}
