8a20524e87d5474d4a1cf04806afd9afa0682594
SW-Muriu <sw.muriu@gmail.com>
2023-05-11 12:48:38 +0300

    Routing advancement

--- RF-TEST003/src/app/administration/administration-routing.module.ts
+++ RF-TEST003/src/app/administration/administration-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdministrationComponent } from './administration.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
 
-//const routes: Routes = [{ path: 'Dashboard', component: DashboardComponent }];
+// const routes: Routes =
+// [{
+//  path: 'Dashboard', component: DashboardComponent
+//  }];
 
 @NgModule({
   imports: [],
--- RF-TEST003/src/app/authentication/authentication-routing.module.ts
+++ RF-TEST003/src/app/authentication/authentication-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication.component';
+import { DashboardComponent }  from './dashboard.component'
 
-const routes: Routes = [{ path: '', component: AuthenticationComponent }];
+const routes: Routes =
+[
+{ path: '', component: AuthenticationComponent },
+{ path: 'Dashboard', component:  DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
